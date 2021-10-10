85edc28d8fe9a2d0c8d0215839c259c3fd27bb1e
Carles Rafols <41967813+karb94@users.noreply.github.com>
2021-10-11 00:52:31 +0100

    Fast push

---
+++ scripts/update_repo.sh
@@ -0,0 +1,29 @@
+#! /bin/sh
+
+script_path=$(dirname $0)
+
+rm -f "$script_path"/../x86_64/*
+repo-add "$script_path"/../x86_64/system_config.db.tar.gz
+# build packages
+for d in "$script_path"/../builds/*
+do
+  (
+    cd "$d" &&
+    makepkg \
+      --force \
+      --nodeps \
+      --clean \
+      --cleanbuild \
+      PKGDEST=../../x86_64
+    rm -rf "$d"/src
+  )
+done
+
+# add packages to database
+cd ../x86_64/
+repo-add -R *db.tar.gz *pkg.tar.zst
+
+# updated repo
+git add "$script_path"/../x86_64/*.pkg.tar.zst
+git commit -am 'New repo update'
+git push
