2717bfdd6ee2d0318abeadb5a58191a81a89d2c1
Carles Rafols <41967813+karb94@users.noreply.github.com>
2022-06-15 01:03:38 +0100

    New repo update

--- builds/aurutils-conf/aur.sh
+++ builds/aurutils-conf/aur.sh
@@ -1,5 +1,5 @@
 export AUR_REPO=aur
-export AUR_DBROOT=/var/cache/pacman/aur/
+export AUR_DBROOT=/var/cache/pacman/pkg/
 export AURDEST=/tmp
 export AUR_PAGER=/usr/bin/nvim
 export AUR_CONFIRM_PAGER=true
